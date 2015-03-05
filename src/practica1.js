76adb11c641cbc5c425fe3980819ffc3e00586be
hecoding <hlaria@ucm.es>
2015-03-05 16:00:07 +0100

    trabajando

--- src/practica1.js
+++ src/practica1.js
@@ -1,38 +1,51 @@
+var Card = function (sprite) {
+		this.nombre = sprite;
+		this.estado = this.states.BOCA_ABAJO;
+	};
+
+// este objeto con prototype porque, al contrario que MemoryGame, se van a crear varias cartas y así comparten código
+Card.prototype = {
+	states : {
+		BOCA_ABAJO : 0,
+		BOCA_ARRIBA : 1,
+		ENCONTRADA : 2
+	},
+
+	flip : function() {
+		if (this.estado === this.BOCA_ABAJO)
+			this.estado = this.BOCA_ARRIBA;
+
+		else if (this.estado === this.BOCA_ARRIBA)
+			this.estado = this.BOCA_ABAJO;
+	},
+
+	found : function() {
+		this.estado = this.ENCONTRADA;
+	},
+
+	compareTo : function (otherCard) {
+		return this.nombre == otherCard.nombre;
+	},
+
+	draw : function (gs, pos) {
+		gs.draw (this.nombre , pos); // no estoy seguro de esto
+	}
+};
+
 var MemoryGame = function (gs) {
 	this.gs = gs;
-	this.cards = new Array (); // poner las cartas
+	this.cards = new Array( new Card ("8-ball"), new Card ("8-ball"),
+							new Card ("back"), new Card ("back"),
+							new Card ("potato"), new Card ("potato"),
+							new Card ("dinosaur"), new Card ("dinosaur"),
+							new Card ("kronos"), new Card ("kronos"),
+							new Card ("rocket"), new Card ("rocket"),
+							new Card ("unicorn"), new Card ("unicorn"),
+							new Card ("guy"), new Card ("guy"),
+							new Card ("zeppelin"), new Card ("zeppelin") );
+	// this.cards.shuffle();
 	this.finished = false;
-	this.messageState = "Welcome"; // mirar el mensaje que se muestra
-
-	this.Card = function (sprite) {
-		this.nombre = sprite;
-		this.estado = this.BOCA_ABAJO;
-	};
-
-	// este objeto con prototype porque, al contrario que MemoryGame, se van a crear varias cartas y así comparten código
-	Card.prototype = {
-		this.BOCA_ABAJO = 0, BOCA_ARRIBA = 1, ENCONTRADA = 2;
-
-		flip : function() {
-			if (this.estado === this.BOCA_ABAJO)
-				this.estado = this.BOCA_ARRIBA;
-
-			else if (this.estado === this.BOCA_ARRIBA)
-				this.estado = this.BOCA_ABAJO;
-		},
-
-		found : function() {
-			this.estado = this.ENCONTRADA;
-		},
-
-		compareTo : function (otherCard) {
-			return this.nombre == otherCard.nombre;
-		},
-
-		draw : function (gs, pos) {
-			gs.draw (this.nombre , pos); // no estoy seguro de esto
-		}
-	};
+	this.messageState = "Memory Game";
 
 	this.initGame = function() {
 		this.gs.load ()
@@ -74,6 +87,8 @@ var MemoryGame = function (gs) {
 		}
 	};
 };
+
+
 
 // cómo hacer el enum de Card (no se puede declarar en prototype?)
 // cómo poner el array de cartas
