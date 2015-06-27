181782b7a62a9b170c0af05660b1883b24d5c9c5
hecoding <hlaria@ucm.es>
2015-06-27 23:57:43 +0200

    working on mario

--- practica3/game.js
+++ practica3/game.js
@@ -10,35 +10,63 @@ var game = function () {
 
 
 
-	Q.loadTMX("level.tmx, mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json", function() {
+	Q.loadTMX("level.tmx, mario_small.png, mario_small.json, goomba.png, goomba.json, bloopa.png, bloopa.json, coin.png, coin.json", function() {
 		Q.compileSheets("mario_small.png","mario_small.json");
 		Q.compileSheets("goomba.png", "goomba.json");
 		Q.compileSheets("bloopa.png", "bloopa.json");
-		Q.load("mainTitle.png", function() {
+		Q.compileSheets("coin.png", "coin.json");
+		Q.load("mainTitle.png, princess.png", function() {
+			Q.animations("mario", {
+				run_right: {frames: [1, 2, 3], rate: 1/7},
+				run_left: {frames: [15, 16, 17], rate: 1/7},
+				stand_right: {frames: [0]},
+				stand_left: {frames: [14]},
+				jump_right: {frames: [4], rate: 1/15},
+				jump_left: {frames: [18], rate: 1/15},
+				die_right: {frames: [12]},
+				die_left: {frames: [26]}
+			});
+
+			Q.animations("goomba", {
+				run: {frames: [0, 1], rate: 1/4},
+				die: {frames: [2]}
+			});
+
+			Q.animations("bloopa", {
+				run: {frames: [0, 1], rate: 1/4},
+				die: {frames: [2]}
+			});
+
+			Q.animations("coin", {
+				shine: {frames: [0, 1, 2], rate: 1/4}
+			});
+
 			Q.stageScene("mainTitle", 0);
 		});
 	});
 
-	// asynchronous load, it's a bad practice to code it like that
-	Q.load("princess.png");
-
 	Q.Sprite.extend("Mario", {
 		init: function(p) {
 			this._super(p, {
+				sprite: "mario",
 				sheet: "marioR",
 				x: 150,
 				y: 380
 			});
 
-			this.add("2d, platformerControls");
+			this.add("2d, platformerControls, animation");
 		},
 
 		step: function(dt) {
-			if(Q.inputs['left'] && this.p.direction == 'right')
-				this.p.flip = 'x';
-			
-			if(Q.inputs['right']  && this.p.direction == 'left')
-				this.p.flip = false;
+			if(this.p.vy == 0) {
+				if (this.p.vx > 0)
+					this.play("run_right");
+				else if (this.p.vx < 0)
+					this.play("run_left");
+				else
+					this.play("stand_" + this.p.direction);
+			}
+			else this.play("jump_" + this.p.direction);
 
 			// check bounds
 			if (this.p.y > 600)
@@ -54,13 +82,15 @@ var game = function () {
 	Q.Sprite.extend("Goomba", {
 		init: function(p) {
 			this._super({
+				sprite: "goomba",
 				sheet: "goomba",
 				x: 350,
 				y: 500,
 				vx: 100
 			});
 
-			this.add("2d, aiBounce");
+			this.add("2d, aiBounce, animation");
+			this.play("run");
 
 			this.on("bump.left,bump.right,bump.bottom", function (collision) {
 				if(collision.obj.isA("Mario")) { 
@@ -75,20 +105,20 @@ var game = function () {
 					collision.obj.p.vy = -300;
 				}
 			});
-		},
-
-		step: function(dt) { }
+		}
 	});
 
 	Q.Sprite.extend("Bloopa", {
 		init: function(p) {
 			this._super({
+				sprite: "bloopa",
 				sheet: "bloopa",
 				x: 550,
 				y: 500
 			});
 		
-			this.add('2d');
+			this.add('2d, animation');
+			this.play("run");
 
 			this.on("bump.left,bump.right,bump.bottom, bump.bottom", function (collision) {
 				if(collision.obj.isA("Mario")) { 
@@ -128,9 +158,29 @@ var game = function () {
 				this.sensorActivated = true;
 				Q.stageScene("winGame", 1);
 			}
-		},
+		}
+	});
 
-		step: function(dt) { }
+	Q.Sprite.extend("Coin", {
+		init: function(p) {
+			this._super({
+				sprite: "coin",
+				sheet: "coin",
+				x: 100,
+				y: 460,
+				gravityY: 0
+			});
+		
+			this.add('2d, animation');
+			this.play("shine");
+
+			this.on("bump.left,bump.right,bump.bottom, bump.bottom, bump.top", function (collision) {
+				if(collision.obj.isA("Mario")) { 
+					this.destroy();
+					// +1 mario. Guardar monedas en Q.state
+				}
+			});
+		}
 	});
 
 	Q.scene("mainTitle", function(stage) {
@@ -152,6 +202,7 @@ var game = function () {
 		stage.insert( new Q.Goomba() );
 		stage.insert( new Q.Bloopa() );
 		stage.insert( new Q.Princess() );
+		stage.insert( new Q.Coin() );
 
 		// set the camera
 		stage.viewport.offsetY = 155;
